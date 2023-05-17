b667bb69409d371839e48547c4b9ebcd1a1af5db
Jirka Daněk <jirka.danek@jdanek.eu>
2023-05-17 11:33:34 +0200

    Initial commit (thanks to CX for the basis of the template)

---
+++ plugins/templates/minimalistic/labels/en.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    'default.label' => 'default',
+	'column_left.label' => 'column_left',
+    'column_left.slot.left' => 'left column',
+	'column_left_cb.label' => 'column_left_cb',
+    'column_left_cb.slot.left' => 'left column',
+	'column_right.label' => 'column_right',
+    'column_right.slot.right' => 'right column',
+	'column_right_cb.label' => 'column_right_cb',
+    'column_right_cb.slot.right' => 'right column',
+	'full_column_left.label' => 'full_column_left',
+    'full_column_left.slot.left' => 'left column',
+	'full_column_left_cb.label' => 'full_column_left_cb',
+    'full_column_left_cb.slot.left' => 'left column',
+	'full_column_right.label' => 'full_column_right',
+    'full_column_right.slot.right' => 'right column',
+	'full_column_right_cb.label' => 'full_column_right_cb',
+    'full_column_right_cb.slot.right' => 'right column',
+];
