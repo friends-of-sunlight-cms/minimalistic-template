8a9e16618d6dbf58a39f7a56033434a32d667ded
Jirka Daněk <jirka.danek@jdanek.eu>
2023-05-18 11:31:58 +0200

    Add translation for config

---
+++ plugins/templates/minimalistic/resources/lang/cs.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    'menu_start' => 'MENU: Minimální pořadové číslo',
+    'menu_end' => 'MENU: Maximální pořadové číslo',
+    'breadcrumb_caption' => 'Navigace',
+    'show_breadcrumbs' => 'Zobrazit drobečkovou navigaci',
+];
---
+++ plugins/templates/minimalistic/resources/lang/en.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    'menu_start' => 'MENU: Minimum order number',
+    'menu_end' => 'MENU: Maximum order number',
+    'breadcrumb_caption' => 'Navigation',
+    'show_breadcrumbs' => 'Show breadcrumbs',
+];
