b2bdf9c585889becc3b7c36fab036d9e92b45d25
Jirka Daněk <jirka.danek@jdanek.eu>
2023-05-18 11:28:58 +0200

    Add configuration

---
+++ plugins/templates/minimalistic/ConfigAction.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace SunlightTemplate\Minimalistic;
+
+use Sunlight\Core;
+use Sunlight\Localization\LocalizationDirectory;
+use Sunlight\Plugin\Action\ConfigAction as BaseConfigAction;
+use Sunlight\Plugin\Plugin;
+use Sunlight\Util\Form;
+use SunlightTemplate\Chameleon\TemplatePlugin;
+
+class ConfigAction extends BaseConfigAction
+{
+    public const THEME_ID = 'minimalistic';
+
+    /** @var $plugin TemplatePlugin */
+    protected $plugin;
+
+    public function __construct(Plugin $plugin)
+    {
+        $this->plugin = $plugin;
+        // register lang for administration
+        Core::$dictionary->registerSubDictionary(self::THEME_ID, new LocalizationDirectory(__DIR__ . DIRECTORY_SEPARATOR . 'resources/lang/'));
+        parent::__construct($plugin);
+    }
+
+    protected function getFields(): array
+    {
+        // config instance
+        $config = $this->plugin->getConfig();
+        return [
+            // menu
+            'menu_start' => [
+                'label' => _lang(self::THEME_ID . '.menu_start'),
+                'input' => '<input type="number" min="0" name="config[menu_start]" value="' . $config['menu_start'] . '" class="inputmini">',
+                'type' => 'text'
+            ],
+            'menu_end' => [
+                'label' => _lang(self::THEME_ID . '.menu_end'),
+                'input' => '<input type="number" min="0" name="config[menu_end]" value="' . $config['menu_end'] . '" class="inputmini">',
+                'type' => 'text'
+            ],
+            'show_breadcrumbs' => [
+                'label' => _lang(self::THEME_ID . '.show_breadcrumbs'),
+                'input' => '<input type="checkbox" name="config[show_breadcrumbs]" value="1"' . Form::activateCheckbox($config['show_breadcrumbs']) . '>',
+                'type' => 'checkbox'
+            ],
+        ];
+    }
+}
